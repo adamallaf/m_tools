9710e706d11a28b9c431021d546e869238684742
Adam <adam.allaf@hotmail.com>
2018-04-27 11:04:31 +0200

    Updated get ip script

--- bash_scripts/get_ip.sh
+++ bash_scripts/get_ip.sh
@@ -1,5 +1,13 @@
-MYIP=$(ifconfig 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://')
-MPREFIX='127.0.0.1'
-MYIP=${MYIP##$MPREFIX}
-MYIP=${MYIP%%$MPREFIX}
-echo ${MYIP}
+if [[ "$OSTYPE" == "linux-android" ]]; then
+    MYIP=$(ifconfig 2>/dev/null | awk '/inet addr:/ {print$2}' | sed's/addr://')
+    MPREFIX='127.0.0.1'
+    MYIP=${MYIP##$MPREFIX}
+    MYIP=${MYIP%%$MPREFIX}
+    echo ${MYIP}
+elif [[ "$OSTYPE" == "linux-gnueabihf" ]]; then
+    MYIP=$(networkctl status 2>/dev/null | grep Address)
+    MPREFIX='^Address:'
+    MPOSTFIX='on*'
+    MYIP=${MYIP%%$MPOSTFIX}
+    echo ${MYIP} | tail -c +10
+fi
